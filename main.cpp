35c3b6dadfaa6bc17152d4dd38e6326b2e3e4ad4
charitha2901 <charitha2929@gmail.com>
2026-05-24 19:53:34 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,25 @@
 int main() {
 
     Calculator calc;
+    int choice;
 
-    std::cout << "Addition: "
-              << calc.add(10, 5)
-              << std::endl;
+    while (true) {
+        std::cout << "\n1. Add\n2. Exit\nEnter choice: ";
+        std::cin >> choice;
+
+        if (choice == 2) {
+            std::cout << "Exiting...\n";
+            break;
+        }
+
+        if (choice == 1) {
+            int a, b;
+            std::cout << "Enter two numbers: ";
+            std::cin >> a >> b;
+
+            std::cout << "Result: " << calc.add(a, b) << std::endl;
+        }
+    }
 
     return 0;
 }
